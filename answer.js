4b9d56712ce99e1e7e693a65b9c6e7e683b07bd6
peterxkl <32321026+peterxkl@users.noreply.github.com>
2019-06-23 18:24:33 +0800

    Create answer.js

---
+++ answer.js
@@ -0,0 +1,14 @@
+//第一题
+var user={};
+user.name="John";
+user.surname="Mike";
+user.name="Peter";
+delete user.name;
+
+//第二题
+var fruit={
+	apple:20,
+	pear:20,
+	peach:10
+};
+console.log(fruit.apple+fruit.pear+fruit.peach);
